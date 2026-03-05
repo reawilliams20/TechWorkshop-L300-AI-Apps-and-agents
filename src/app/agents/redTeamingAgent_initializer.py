587ff494652453aef9f4c184809e70fa1c9a8569
Andrea Ngoka-Williams <58191911+reawilliams20@users.noreply.github.com>
2026-03-05 14:05:25 +0000

    Add Red Teaming Agent initialization and custom attack prompts for risk assessment

---
+++ src/app/agents/redTeamingAgent_initializer.py
@@ -0,0 +1,54 @@
+# Azure imports
+from azure.identity import DefaultAzureCredential
+from azure.ai.evaluation.red_team import RedTeam, RiskCategory, AttackStrategy
+from pyrit.prompt_target import OpenAIChatTarget
+import os
+import asyncio
+from dotenv import load_dotenv
+load_dotenv()
+
+# Azure AI Project Information
+azure_ai_project = os.getenv("FOUNDRY_ENDPOINT")
+
+# Instantiate your AI Red Teaming Agent
+# red_team_agent = RedTeam(
+#     azure_ai_project=azure_ai_project,
+#     credential=DefaultAzureCredential(),
+#     risk_categories=[
+#         RiskCategory.Violence,
+#         RiskCategory.HateUnfairness,
+#         RiskCategory.Sexual,
+#         RiskCategory.SelfHarm
+#     ],
+#     num_objectives=5,
+# )
+
+red_team_agent = RedTeam(
+    azure_ai_project=azure_ai_project,
+    credential=DefaultAzureCredential(),
+    custom_attack_seed_prompts="data/custom_attack_prompts.json",
+)
+
+
+# Configuration for Azure OpenAI model
+chat_target = OpenAIChatTarget(
+    model_name=os.environ.get("gpt_deployment"),
+    endpoint=os.environ.get("gpt_endpoint"),
+    api_key=os.environ.get("FOUNDRY_KEY")
+)
+ 
+
+
+async def main():
+    red_team_result = await red_team_agent.scan(
+        target=chat_target,
+        scan_name="Red Team Scan - Easy-Moderate Strategies",
+        attack_strategies=[
+            AttackStrategy.Flip,
+            AttackStrategy.ROT13,
+            AttackStrategy.Base64,
+            AttackStrategy.AnsiAttack,
+            AttackStrategy.Tense
+        ])
+
+asyncio.run(main())
